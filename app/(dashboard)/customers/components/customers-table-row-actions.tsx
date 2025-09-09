3f6c73504cd2fd3afc6514b7ea8bee82bd1f795c
shahreazebd <shahreaz@evidentbd.com>
2025-09-10 02:08:10 +0600

    cusotmer action

--- app/(dashboard)/customers/components/customers-table-row-actions.tsx
+++ app/(dashboard)/customers/components/customers-table-row-actions.tsx
@@ -2,21 +2,31 @@
 
 import { Row } from "@tanstack/react-table"
 import { MoreHorizontal } from "lucide-react"
+import React from "react"
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { customerStatus } from "../data/data"
 
 interface CustomersTableRowActionsProps {
   row: Row<any>
 }
 
 export function CustomersTableRowActions({ row }: CustomersTableRowActionsProps) {
+  const [position, setPosition] = React.useState("true")
+
   function deleteCustomerHandler(id: string) {}
 
   return (
@@ -35,9 +45,23 @@ export function CustomersTableRowActions({ row }: CustomersTableRowActionsProps)
         >
           Edit
         </DropdownMenuItem>
-        <DropdownMenuItem>Make a copy</DropdownMenuItem>
-        <DropdownMenuItem>Favorite</DropdownMenuItem>
+
         <DropdownMenuSeparator />
+
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>Status</DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                {customerStatus.map((s) => (
+                  <DropdownMenuRadioItem value={s.value}>
+                    <s.icon /> {s.label}
+                  </DropdownMenuRadioItem>
+                ))}
+              </DropdownMenuRadioGroup>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
 
         <DropdownMenuSeparator />
         <DropdownMenuItem
